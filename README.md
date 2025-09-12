492668192045a449bbfd42c5b5e95096fb1b8f2d
Haseebullah kehar <keharhaseebullah@gmail.com>
2025-09-13 02:40:24 +0500

    MLOps

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Youtube-Sentiment-Insights-MLOps
+conda create -n youtube pytho=3.11 -y 
+
+conda activate youtube 
+
+pip install -r requirements.txt 
+
+
+# DVC 
+
+dvc init 
+
+dvc repro 
+
+dvc dag 
+
+
+# AWS 
+
+aws configure 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="youtube",
+    version="0.0.0",
+    author="Haseebullah",
+    author_email='keharhaseebullah@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+
+)
